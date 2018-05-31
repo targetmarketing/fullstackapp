fead848f127ca41a3fe3cd7cc740754b494ee50f
CalijaDj <djordjecaki@live.com>
2018-05-31 13:06:34 +0200

    initial backend

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const createError = require('http-errors')
+const indexRouter = require('./routes/index') 
+const bodyParser = require('body-parser')
+
+
+
+
+
+app.use(express.urlencoded({ extended: false }));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+
+app.use('/',indexRouter)
+
+// catch error
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+
+module.exports = app;
---
+++ bin/www.js
@@ -0,0 +1,46 @@
+const http = require('http')
+const app = require('../app')
+const debug = require('debug')('prvenac:server');
+
+const port = normalizePort(process.env.PORT || '3000')
+app.set('port', port)
+
+
+
+const server = http.createServer(app);
+
+// listen on provided port
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+}
+
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
